369d769c0816f20364bb164a8bca7a55fcf34703
abspayd <abspayd@mtu.edu>
2024-12-30 21:16:21 -0500

    fix: fixes a bug where project directories containing '.' don't switch the client

--- cmd/open.go
+++ cmd/open.go
@@ -43,19 +43,19 @@ var (
 			}
 
 			// replace '.' with '_'
-			project_name = strings.ReplaceAll(project_name, ".", "_")
+			project_title := strings.ReplaceAll(project_name, ".", "_")
 
 			// == Tmux ==
 			// Check if session exists
 			is_attached := len(os.Getenv("TMUX")) > 0
-			session_exists, err := tmuxSessionExists(project_name)
+			session_exists, err := tmuxSessionExists(project_title)
 			if err != nil {
 				return err
 			}
 			if !session_exists {
 				// Create new session
 				project_path := projects[project_name] + "/" + project_name
-				args := []string{"new-session", "-s", project_name, "-c", project_path}
+				args := []string{"new-session", "-s", project_title, "-c", project_path}
 				if is_attached {
 					args = append(args, "-d")
 				}
@@ -68,13 +68,13 @@ var (
 
 			// Check if client is attached
 			if !is_attached {
-				err = tmuxAttach(project_name)
+				err = tmuxAttach(project_title)
 				if err != nil {
 					return err
 				}
 			} else {
 				// Switch to new session
-				shellCmd := exec.Command("tmux", "switch-client", "-t", project_name)
+				shellCmd := exec.Command("tmux", "switch-client", "-t", project_title)
 				err = shellCmd.Run()
 				if err != nil {
 					return err
